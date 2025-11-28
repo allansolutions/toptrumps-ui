aea376bbfc94ea76db254d628e81f8b3b615a33b
WalterM <walter.morales@allansolutions.com>
2025-11-28 12:53:15 -0500

    feat: updated envirotment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
